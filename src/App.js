3980da30255abfe496c9df77b7e991f63122c033
Sandro Santos <sandro.dev@gmail.com>
2019-11-23 18:02:01 -0300

    react-router-dom and routes configuration

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import Routes from './routes';
+import GlobalStyle from './styles/global';
+
 function App() {
-  return (<h1>React is working!</h1>);
+  return (
+		<>
+			<Routes />
+			<GlobalStyle />
+		</>
+	);
 }
 
 export default App;
