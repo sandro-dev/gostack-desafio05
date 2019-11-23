f0f3b7fec386fbddc270e9ee59103fd9bec9c1b1
Sandro Santos <sandro.dev@gmail.com>
2019-11-23 18:46:48 -0300

    main page with methods handleInput and handleSubmit, and styled-components defineds

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import { Container } from './styles';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import { Container, Form, SubmitButton, List } from './styles';
 
-export default function Main() {
-	return (
-		<Container>
-			<h1>Main Page</h1>
-		</Container>
-	);
+export default class Main extends Component {
+	state = {
+		newRepo: '',
+		repositories: [],
+		loading: 0,
+	};
+
+	componentDidMount() {}
+
+	componentDidUpdate() {}
+
+	handleInput = e => {
+		console.log(e.target.value);
+		this.setState({
+			newRepo: e.target.value,
+		});
+	};
+
+	handleSubmit = e => {
+		e.preventDefault();
+		const { repositories, newRepo } = this.state;
+
+		this.setState({ loading: 1 });
+
+		this.setState({
+			repositories: [...repositories, newRepo],
+			newRepo: '',
+		});
+	};
+
+	render() {
+		const { newRepo } = this.state;
+		return (
+			<Container>
+				<h1>
+					<FaGithubAlt />
+					Repositories
+				</h1>
+				<Form onSubmit={this.handleSubmit}>
+					<input
+						type="text"
+						placeholder="Adicionar repositório"
+						onChange={this.handleInput}
+						value={newRepo}
+					/>
+					<SubmitButton>
+						<FaPlus color="#fff" size={14} />
+					</SubmitButton>
+				</Form>
+				<List />
+			</Container>
+		);
+	}
 }
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,3 +1,46 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+	max-width: 700px;
+	min-height: 50vh;
+	margin: 80px auto;
+	background: #fff;
+	border-radius: 4px;
+	box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+	padding: 30px;
+
+	h1 {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		font-size: 26px;
+		font-family: Arial, Helvetica, sans-serif;
+
+		svg {
+			margin-right: 10px;
+		}
+	}
+`;
+
+export const Form = styled.form`
+	margin-top: 20px;
+	display: flex;
+	flex-direction: row;
+
+	input {
+		flex: 1;
+		border: 1px solid #eee;
+		padding: 10px 15px;
+		border-radius: 4px;
+		font-size: 16px;
+	}
+`;
+
+export const SubmitButton = styled.button.attrs({
+	type: 'submit',
+})`
+	background-color: #00c2c9;
+	padding: 0 15px;
+`;
+
+export const List = styled.ul``;
