28751f0acddf9c4867c1cc618d023108ac467492
Sandro Santos <sandro.dev@gmail.com>
2019-11-24 18:52:11 -0300

    Repository structure and style

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,13 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Container from '../../components/Container';
 
-import { Owner, Issues } from './styles';
+import api from '../../services/api';
 
-export default function Repository() {
-	return (
-		<Container>
-			<h1>Repository Page</h1>
-		</Container>
-	);
+import { Loading, Owner, Issues, IssueDescription } from './styles';
+
+export default class Repository extends Component {
+	state = {
+		repository: {},
+		issues: [],
+		loading: true,
+		owner: {},
+	};
+
+	async componentDidMount() {
+		const { match } = this.props;
+
+		const repoName = decodeURIComponent(match.params.repository);
+		console.log('repoName: ', repoName);
+
+		const [repository, issues] = await Promise.all([
+			api.get(`/repos/${repoName}`),
+			api.get(`/repos/${repoName}/issues`),
+		]);
+
+		this.setState({
+			repository: repository.data,
+			issues: issues.data,
+			owner: repository.data.owner,
+			loading: false,
+		});
+
+		console.log('repository', repository.data);
+		console.log('issues', issues.data);
+	}
+
+	render() {
+		const { repository, issues, owner, loading } = this.state;
+
+		if (loading) {
+			return <Loading>Carregando...</Loading>;
+		}
+
+		return (
+			<Container>
+				<Owner>
+					<img src={owner.avatar_url} alt={owner.login} />
+					<h1>{repository.full_name}</h1>
+					<p>{repository.description}</p>
+				</Owner>
+				<Issues>
+					{issues.map(issue => (
+						<li key={issue.id}>
+							<img src={issue.user.avatar_url} alt={repository.owner.login} />
+							<IssueDescription>
+								<h3>{issue.title}</h3>
+								<span>{issue.user.login}</span>
+							</IssueDescription>
+						</li>
+					))}
+				</Issues>
+			</Container>
+		);
+	}
 }
--- src/pages/Repository/styles.js
+++ src/pages/Repository/styles.js
@@ -1,3 +1,70 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Loading = styled.div`
+	font-size: 38px;
+	font-weight: bold;
+	color: #fff;
+	font-family: 'Courier New', Courier, monospace;
+
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 100vh;
+`;
+
+export const Owner = styled.header`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+
+	p {
+		font-family: 'Courier New', Courier, monospace;
+		font-size: 12px;
+		line-height: 1.4;
+		color: #999;
+		width: 400px;
+		text-align: center;
+	}
+
+	img {
+		width: 120px;
+		border-radius: 60px;
+	}
+`;
+
+export const Issues = styled.ul`
+	margin-top: 50px;
+	list-style: none;
+
+	li {
+		padding: 10px 15px;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		border: 2px dashed #eee;
+		border-radius: 4px;
+		margin-top: 10px;
+
+		img {
+			width: 60px;
+			border-radius: 30px;
+			border: 1px solid #eee;
+			margin-right: 10px;
+		}
+	}
+`;
+
+export const IssueDescription = styled.div`
+	h3 {
+		font-family: 'Courier New', Courier, monospace;
+		font-size: 16px;
+		line-height: 1.4;
+		color: #333;
+	}
+
+	span {
+		color: #00c2c9;
+		font-weight: bold;
+		font-size: 14px;
+	}
+`;
